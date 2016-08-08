3dd08a3275a73f947600f6f6cfa6bc5b388e70c4
Vladimir Karpovich <vkarpovich@gmail.com>
2016-08-07 21:01:56 -0500

    Add command line input processing and calls for roman numerical library in main calculator application

--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "roman_calc.h"
 
 /*
@@ -5,7 +9,36 @@
 */
 
 int
-main (void)
+main (int argc, char *argv[])
 {
+  char result[128];
+  if (argc != 4)
+    {
+      fprintf (stderr, "Usage: roman_calc [numeral] +|- [numeral]\n");
+      exit (EXIT_FAILURE);
+    }
+  if (roman_number_check (argv[1]) == ERROR)
+    {
+      fprintf (stderr, "Incorrect Roman numeral '%s'\n", argv[1]);
+      exit (EXIT_FAILURE);
+    }
+
+  if (roman_number_check (argv[3]) == ERROR)
+    {
+      fprintf (stderr, "Incorrect Roman numeral '%s'\n", argv[3]);
+      exit (EXIT_FAILURE);
+    }
+  if (*argv[2] == '+')
+    roman_number_add (argv[1], argv[3], result);
+  else if (*argv[2] == '-')
+    roman_number_sub (argv[1], argv[3], result);
+  else
+    {
+      fprintf (stderr, "Wrong operand '%s'.Only supported + or - .\n",
+	       argv[2]);
+      exit (EXIT_FAILURE);
+    }
+  printf ("%s\n", result);
+
   return 0;
 }
